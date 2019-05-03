f3c9ff37a13530ca3e33fa70496a8c9f9e5e4d3b
Tailor Vijay <tailor.vj@gmail.com>
2019-05-03 04:00:17 +0300

    completed pollution gauge display from Firebase

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
-
+import firebase from './utils/Firebase';
 import Map from './Map';
 import Gauge from './Gauge';
-
-
-import './App.css';
+import PageHeader from './PageHeader';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null, 
-      displayName : null,
-      userID: null,
-      photo: null
-    };
+  constructor(props) {
+    super(props);
+    this.state = {  gaugeValue: 0.1, gaugeColor: 'grey' };
   }
+  componentWillMount(){
+    let espRef = firebase.database().ref('ESP8266_Test');
+    let colorsArray = ['grey', 'blue', 'teal', 'olive', 'green', 'yellow', 'orange', 'purple', 'red', 'red'];
+    espRef.on('child_changed', snapshot => {
+      let gaugeVal = { text: snapshot.val() };
+      this.setState({ gaugeValue: '' });
+      this.setState({ gaugeValue: gaugeVal.text });
+      console.log(Math.floor(gaugeVal.text/10));
+      let myColor = colorsArray[Math.floor(gaugeVal.text/10)];
+      console.log(myColor);
+      this.setState({gaugeColor: `${myColor}`})
+  
+    });
+  }
+
 
   render() {
-    return(
-      <div>
-        test
-        <Map />
-        <Gauge />
-      </div>
+    return (
+        <div>
+          <PageHeader />
+          <div >
+          {/*<div className="ui hidden divider"></div> */}
+            <div className="ui vertical masthead center aligned segment">
+
+            <Gauge gaugeValue={this.state.gaugeValue} gaugeColor={this.state.gaugeColor} />
+            <Map />
+
+            </div>
+          </div>
+        </div>
     );
   }
-
 }
 
 export default App;
